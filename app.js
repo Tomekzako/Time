ccff60a6e3cf6a395dca3f91df3b50a0af4c240f
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-04-10 11:00:31 +0200

    Time

---
+++ app.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM');
+
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+    const timeBlock = document.querySelector('.time');
+    const currentTime = timeBlock.querySelectorAll('h1');
+    const showTime = timeBlock.querySelector('#showTime');
+    const showDay = timeBlock.querySelector('#showDay');
+    const dayOfWeek = timeBlock.querySelector('#dayOfWeek');
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+    for (var i = 0; i < currentTime.length; i++) {
+        currentTime[i].addEventListener('click', function () {
+            var next = this.nextElementSibling;
+            if (next.style.display != 'none') {
+                next.style.display = 'none';
+            } else {
+                next.style.display = 'block';
+            };
+        });
+    }
+
+    function leadingZero(i) {
+        return (i < 10) ? '0' + i : i;
+    }
+    
+    
+    function setDate() {
+        const now = new Date();
+
+        const seconds = now.getSeconds();
+        const secondDegrees = ((seconds / 60) * 360);
+        secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+        const minutes = now.getMinutes();
+        const minutesDegrees = ((minutes / 60) * 360);
+        minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegrees = ((hours / 12) * 360);
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+        
+         showTime.innerHTML = "Now it's: " + leadingZero(now.getHours()) + ":" + leadingZero(now.getMinutes()) + ":" + leadingZero(now.getSeconds());
+
+
+        dayOfWeek.innerHTML = "Today is: " + weekday[now.getDay()];
+        
+    
+        if (hours > 0 && hours < 6) showDay.innerHTML = leadingZero(now.getDate()) + "." + leadingZero((now.getMonth() + 1)) + "." + leadingZero(now.getFullYear()) + " Good night";
+        if (hours >= 6 && hours < 12) showDay.innerHTML = leadingZero(now.getDate()) + "." + leadingZero((now.getMonth() + 1)) + "." + leadingZero(now.getFullYear()) + "Good morning";
+        if (hours >= 12 && hours < 18) showDay.innerHTML = leadingZero(now.getDate()) + "." + leadingZero((now.getMonth() + 1)) + "." + leadingZero(now.getFullYear()) + " - " + "Good afternoon";
+        if (hours >= 19 && hours < 24) showDay.innerHTML = leadingZero(now.getDate()) + "." + leadingZero((now.getMonth() + 1)) + "." + leadingZero(now.getFullYear()) + " - " + "Good evening";
+        
+        
+        
+        
+        
+    }
+
+    setInterval(setDate, 1000);
+
+});
